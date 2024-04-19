e105ccd8064e8762ee082320dddb5da826c9f758
Diego Cardoso <90847079+diegoCBorba@users.noreply.github.com>
2024-04-18 21:30:49 -0300

    Error page

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -1,6 +1,8 @@
+import error from "./ErrorPage.module.css"
+
 export const ErrorPage = () => {
   return (
-    <div>
+    <div className={error.container}>
       <h1>Error 404</h1>
     </div>
   )
