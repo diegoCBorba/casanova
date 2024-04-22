5f179c785a665d8093eddda6d007f2b65bb04c25
Diego Cardoso <90847079+diegoCBorba@users.noreply.github.com>
2024-04-22 14:46:35 -0300

    Mudanças visuais

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -21,9 +21,9 @@ export const Services = () => {
               </div>
               <h2>Planejados Residenciais</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sed harum! Asperiores accusamus vero autem?</p>
-              <svg className={service.animateSvg}>
+              {/* <svg className={service.animateSvg}>
                 <rect x="0" y="0" fill="none" width="100%" height="100%"/>
-              </svg>
+              </svg> */}
               <Link to="/" className={service.iconShare}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg>
               </Link>
@@ -34,9 +34,9 @@ export const Services = () => {
               </div>
               <h2>Planejados Comerciais</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sed harum! Asperiores accusamus vero autem?</p>
-              <svg className={service.animateSvg}>
+              {/* <svg className={service.animateSvg}>
                 <rect x="0" y="0" fill="none" width="100%" height="100%"/>
-              </svg>
+              </svg> */}
               <Link to="/" className={service.iconShare}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg>
               </Link>
