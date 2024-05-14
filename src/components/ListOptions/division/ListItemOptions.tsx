52940bc32b3a77fcb3fad0e7bd41dc7ef230fc0d
Diego Cardoso <90847079+diegoCBorba@users.noreply.github.com>
2024-05-14 15:11:18 -0300

    Lógica opções

--- src/components/ListOptions/division/ListItemOptions.tsx
+++ src/components/ListOptions/division/ListItemOptions.tsx
@@ -4,11 +4,15 @@ import { ReactNode } from "react"
 interface PropsListItem{
   href: string,
   children?: ReactNode
+  isActive?: boolean
 }
 
 export const ListItemOptions = (props: PropsListItem) => {
   return (
-    <li className={options.listItem}>
+    <li className={`
+      ${options.listItem}
+      ${props.isActive? options.active : ""}
+    `}>
       <a href={props.href}>{props.children}</a>
     </li>
   )
--- src/routes/Contact/Contact.tsx
+++ src/routes/Contact/Contact.tsx
@@ -1,9 +1,17 @@
 import "./Contact.css"
 
 import { TitlePages } from "../../components/Title/TitlePages"
+import { ListOptions } from "../../components/ListOptions/ListOptions"
+import { ListItemOptions } from "../../components/ListOptions/division/ListItemOptions"
 
 export const Contact = () => {
   return (
-    <TitlePages title="Contatos"/>
+    <>
+      <TitlePages title="Contatos"/>
+      <ListOptions>
+        <ListItemOptions isActive={true} href="#service">Atendimento</ListItemOptions>
+        <ListItemOptions href="#doubts">Dúvidas Frequentes</ListItemOptions>
+      </ListOptions> 
+    </>
   )
 }
