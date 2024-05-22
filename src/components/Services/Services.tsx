d8e8b144daedfb91dd2985c4c4544d7ae41efa44
Diego Cardoso <90847079+diegoCBorba@users.noreply.github.com>
2024-05-22 15:42:04 -0300

    Update Services.tsx

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -11,7 +11,7 @@ export const ServicesHome = () => {
           <div className={service.boxAbout}>
             <Title>Serviços</Title>
             <h1>Nossa Tipologia de Serviços</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, odio?</p>
+            <p>Transformamos seu espaço comercial em um ambiente funcional e elegante.</p>
             <Link to="/services" className="btn-flip2" data-back="Serviços" data-front="Saiba mais"/>
           </div>
           <div className={service.containerLinks}>
@@ -20,7 +20,7 @@ export const ServicesHome = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-hotel"><path d="M10 22v-6.57"/><path d="M12 11h.01"/><path d="M12 7h.01"/><path d="M14 15.43V22"/><path d="M15 16a5 5 0 0 0-6 0"/><path d="M16 11h.01"/><path d="M16 7h.01"/><path d="M8 11h.01"/><path d="M8 7h.01"/><rect x="4" y="2" width="16" height="20" rx="2"/></svg>
               </div>
               <h2>Planejados Residenciais</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sed harum! Asperiores accusamus vero autem?</p>
+              <p>Móveis personalizados que otimizam cada canto da sua casa, unindo estética e praticidade para criar o lar dos seus sonhos.</p>
               {/* <svg className={service.animateSvg}>
                 <rect x="0" y="0" fill="none" width="100%" height="100%"/>
               </svg> */}
@@ -33,7 +33,7 @@ export const ServicesHome = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-landmark"><line x1="3" x2="21" y1="22" y2="22"/><line x1="6" x2="6" y1="18" y2="11"/><line x1="10" x2="10" y1="18" y2="11"/><line x1="14" x2="14" y1="18" y2="11"/><line x1="18" x2="18" y1="18" y2="11"/><polygon points="12 2 20 7 4 7"/></svg>
               </div>
               <h2>Planejados Comerciais</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sed harum! Asperiores accusamus vero autem?</p>
+              <p>Soluções sob medida que combinam funcionalidade e design para transformar seu espaço de trabalho em um ambiente inspirador e eficiente.</p>
               {/* <svg className={service.animateSvg}>
                 <rect x="0" y="0" fill="none" width="100%" height="100%"/>
               </svg> */}
